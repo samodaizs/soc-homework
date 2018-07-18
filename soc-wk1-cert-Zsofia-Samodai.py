1462122f412bb5fe6cdb7c99a861063dff06a0d5
samodaizs <41268386+samodaizs@users.noreply.github.com>
2018-07-18 14:48:26 -0600

    Add files via upload

---
+++ soc-wk1-cert-Zsofia-Samodai.py
@@ -0,0 +1,13 @@
+# How many hours are in a year?
+print(24 * 365)
+
+# How many minutes are in a decade?
+print(((60 * 24) * 365.25) * 10)
+
+# How many seconds old are you?
+
+print((60*60*24*365*22)+(60*60*24*366*7)+(60*60*24*273))
+
+# Calculate Andreea Visanoiu's age.
+
+print(48618000/(60*60*24*365.25))
